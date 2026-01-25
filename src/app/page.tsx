81027a244540a94562d61cd8d82a7df34fc1a5ed
Jannis Schaefer <jannis@schaefer.dev>
2026-01-25 14:21:25 +0100

    chore: setting up auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import Image from "next/image";
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import { SignIn } from "@/app/SignIn";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignIn />
       {tasks?.map(({ _id, text }) => (
         <div key={_id}>{text}</div>
       ))}
---
+++ src/convex/schema.ts
@@ -0,0 +1,19 @@
+import { defineSchema, defineTable } from "convex/server";
+import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
+
+const schema = defineSchema(
+  {
+    ...authTables,
+    tasks: defineTable({
+      isCompleted: v.boolean(),
+      text: v.string(),
+    }),
+    // Your other tables...
+  },
+  {
+    schemaValidation: true,
+  },
+);
+
+export default schema;
